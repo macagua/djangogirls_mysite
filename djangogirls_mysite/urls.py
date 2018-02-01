6ac0d1ae7b37db40a4a67550409d99810df925a6
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-02-01 15:54:23 -0400

    Changed the  HTML template.

--- djangogirls_mysite/urls.py
+++ djangogirls_mysite/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'djangogirls_mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
