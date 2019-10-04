c154c79959d3832c8996ed1a49355fd576205f77
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2019-10-04 16:08:14 -0400

    Added django url support for pythonanywhere deploy

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    # ex: /
+    url(r'^$', views.post_list, name='post_list'),
+    # ex: /post/1/
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    # ex: /post/new/
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    # ex: /post/1/edit/
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
